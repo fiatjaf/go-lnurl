4444b02837b94030c3737ba44cea5381f74c3a28
fiatjaf <fiatjaf@gmail.com>
2021-10-06 08:23:20 -0300

    refactor again to fix this metadata stuff once more.

--- pay.go
+++ pay.go
@@ -59,18 +59,27 @@ func AESAction(description string, preimage []byte, content string) (*SuccessAct
 
 type LNURLPayParams struct {
 	LNURLResponse
-	Callback       string        `json:"callback"`
-	Tag            string        `json:"tag"`
-	MaxSendable    int64         `json:"maxSendable"`
-	MinSendable    int64         `json:"minSendable"`
-	Metadata       Metadata      `json:"metadata"`
-	CommentAllowed int64         `json:"commentAllowed"`
-	PayerData      PayerDataSpec `json:"payerData,omitempty"`
+	Callback        string        `json:"callback"`
+	Tag             string        `json:"tag"`
+	MaxSendable     int64         `json:"maxSendable"`
+	MinSendable     int64         `json:"minSendable"`
+	EncodedMetadata string        `json:"metadata"`
+	CommentAllowed  int64         `json:"commentAllowed"`
+	PayerData       PayerDataSpec `json:"payerData,omitempty"`
+
+	Metadata Metadata `json:"-"`
 }
 
-func (params LNURLPayParams) CallbackURL() *url.URL {
-	parsed, _ := url.Parse(params.Callback)
-	return parsed
+type Metadata struct {
+	Description     string
+	LongDescription string
+	Image           struct {
+		DataURI string
+		Bytes   []byte
+		Ext     string
+	}
+	LightningAddress string
+	IsEmail          bool
 }
 
 type PayerDataSpec struct {
@@ -79,10 +88,6 @@ type PayerDataSpec struct {
 	LightningAddress *PayerDataItemSpec    `json:"identifier"`
 	Email            *PayerDataItemSpec    `json:"email"`
 	KeyAuth          *PayerDataKeyAuthSpec `json:"auth"`
-}
-
-func (s PayerDataSpec) Exists() bool {
-	return s.FreeName != nil || s.PubKey != nil || s.LightningAddress != nil || s.Email != nil || s.KeyAuth != nil
 }
 
 type PayerDataItemSpec struct {
@@ -128,6 +133,15 @@ type SuccessAction struct {
 	IV          string `json:"iv,omitempty"`
 }
 
+func (params LNURLPayParams) CallbackURL() *url.URL {
+	parsed, _ := url.Parse(params.Callback)
+	return parsed
+}
+
+func (s PayerDataSpec) Exists() bool {
+	return s.FreeName != nil || s.PubKey != nil || s.LightningAddress != nil || s.Email != nil || s.KeyAuth != nil
+}
+
 func (sa *SuccessAction) Decipher(preimage []byte) (content string, err error) {
 	ciphertext, err := base64.StdEncoding.DecodeString(sa.Ciphertext)
 	if err != nil {
@@ -150,16 +164,47 @@ func (sa *SuccessAction) Decipher(preimage []byte) (content string, err error) {
 func (_ LNURLPayParams) LNURLKind() string { return "lnurl-pay" }
 
 func HandlePay(j gjson.Result) (LNURLParams, error) {
-	strmetadata := j.Get("metadata").String()
-	var metadata Metadata
-	err := json.Unmarshal([]byte(strmetadata), &metadata)
+	var params LNURLPayParams
+	err := json.Unmarshal([]byte(j.Raw), &params)
 	if err != nil {
 		return nil, err
 	}
 
-	callback := j.Get("callback").String()
+	// parse metadata
+	strmetadata := j.Get("metadata").String()
+	var metadata Metadata
+	var array []interface{}
+	if err := json.Unmarshal([]byte(params.EncodedMetadata), &array); err != nil {
+		return params, err
+	}
+	for _, item := range array {
+		entry, _ := item.([]interface{})
+		if len(entry) <= 1 {
+			continue
+		}
+
+		switch entry[0] {
+		case "text/plain":
+			params.Metadata.Description, _ = entry[1].(string)
+		case "text/long-desc":
+			params.Metadata.LongDescription, _ = entry[1].(string)
+		case "image/png;base64", "image/jpeg;base64":
+			k, _ := entry[0].(string)
+			v, _ := entry[1].(string)
+
+			params.Metadata.Image.DataURI = "data:" + k + "," + v
+			params.Metadata.Image.Bytes, _ = base64.StdEncoding.DecodeString(v)
+			params.Metadata.Image.Ext = strings.Split(strings.Split(k, "/")[1], ";")[0]
+		case "text/email", "text/identifier":
+			params.Metadata.LightningAddress, _ = entry[1].(string)
+			if entry[0].(string) == "text/email" {
+				params.Metadata.IsEmail = true
+			}
+		}
+	}
 
 	// parse url
+	callback := j.Get("callback").String()
 	callbackURL, err := url.Parse(callback)
 	if err != nil {
 		return nil, errors.New("callback is not a valid URL")
@@ -175,13 +220,14 @@ func HandlePay(j gjson.Result) (LNURLParams, error) {
 	json.Unmarshal([]byte(j.Get("payerData").String()), &payerData)
 
 	return LNURLPayParams{
-		Tag:            "payRequest",
-		Callback:       callbackURL.String(),
-		Metadata:       metadata,
-		MaxSendable:    j.Get("maxSendable").Int(),
-		MinSendable:    j.Get("minSendable").Int(),
-		CommentAllowed: j.Get("commentAllowed").Int(),
-		PayerData:      payerData,
+		Tag:             "payRequest",
+		Callback:        callbackURL.String(),
+		EncodedMetadata: strmetadata,
+		Metadata:        metadata,
+		MaxSendable:     j.Get("maxSendable").Int(),
+		MinSendable:     j.Get("minSendable").Int(),
+		CommentAllowed:  j.Get("commentAllowed").Int(),
+		PayerData:       payerData,
 	}, nil
 }
 
@@ -190,6 +236,32 @@ func (params LNURLPayParams) Call(
 	comment string,
 	payerdata *PayerDataValues,
 ) (*LNURLPayValues, error) {
+	if params.PayerData.Email != nil &&
+		params.PayerData.Email.Mandatory &&
+		(payerdata == nil || payerdata.Email == "") {
+		return nil, fmt.Errorf("email is mandatory")
+	}
+	if params.PayerData.LightningAddress != nil &&
+		params.PayerData.LightningAddress.Mandatory &&
+		(payerdata == nil || payerdata.LightningAddress == "") {
+		return nil, fmt.Errorf("lightning address is mandatory")
+	}
+	if params.PayerData.FreeName != nil &&
+		params.PayerData.FreeName.Mandatory &&
+		(payerdata == nil || payerdata.FreeName == "") {
+		return nil, fmt.Errorf("name is mandatory")
+	}
+	if params.PayerData.PubKey != nil &&
+		params.PayerData.PubKey.Mandatory &&
+		(payerdata == nil || payerdata.PubKey == "") {
+		return nil, fmt.Errorf("pubkey is mandatory")
+	}
+	if params.PayerData.KeyAuth != nil &&
+		params.PayerData.KeyAuth.Mandatory &&
+		(payerdata == nil || payerdata.KeyAuth == nil) {
+		return nil, fmt.Errorf("auth is mandatory")
+	}
+
 	callback := params.CallbackURL()
 
 	qs := callback.Query()
@@ -239,9 +311,9 @@ func (params LNURLPayParams) Call(
 
 	var hhash [32]byte
 	if payerdata != nil && params.PayerData.Exists() {
-		hhash = params.Metadata.HashWithPayerData(payerdataJSON)
+		hhash = params.HashWithPayerData(payerdataJSON)
 	} else {
-		hhash = params.Metadata.Hash()
+		hhash = params.HashMetadata()
 	}
 
 	if inv.DescriptionHash != hex.EncodeToString(hhash[:]) {
@@ -261,96 +333,42 @@ func (params LNURLPayParams) Call(
 	return &values, nil
 }
 
-type Metadata struct {
-	Encoded []byte
-
-	Description     string
-	LongDescription string
-	Image           struct {
-		DataURI string
-		Bytes   []byte
-		Ext     string
-	}
-	LightningAddress string
-	IsEmail          bool
-}
-
-func (m *Metadata) UnmarshalJSON(src []byte) error {
-	m.Encoded = src
-
-	var array []interface{}
-	if err := json.Unmarshal(src, &array); err != nil {
-		return err
-	}
-
-	for _, item := range array {
-		entry, _ := item.([]interface{})
-		if len(entry) <= 1 {
-			continue
-		}
-
-		switch entry[0] {
-		case "text/plain":
-			m.Description, _ = entry[1].(string)
-		case "text/long-desc":
-			m.LongDescription, _ = entry[1].(string)
-		case "image/png;base64", "image/jpeg;base64":
-			k, _ := entry[0].(string)
-			v, _ := entry[1].(string)
-
-			m.Image.DataURI = "data:" + k + "," + v
-			m.Image.Bytes, _ = base64.StdEncoding.DecodeString(v)
-			m.Image.Ext = strings.Split(strings.Split(k, "/")[1], ";")[0]
-		case "text/email", "text/identifier":
-			m.LightningAddress, _ = entry[1].(string)
-			if entry[0].(string) == "text/email" {
-				m.IsEmail = true
-			}
-		}
-	}
-
-	return nil
-}
-
-func (m Metadata) Encode() []byte {
-	if m.Encoded == nil {
+func (params LNURLPayParams) MetadataEncoded() string {
+	if params.EncodedMetadata == "" {
 		raw := make([]interface{}, 0, 5)
-		raw = append(raw, []string{"text/plain", m.Description})
+		raw = append(raw, []string{"text/plain", params.Metadata.Description})
 
-		if m.LongDescription != "" {
-			raw = append(raw, []string{"text/long-desc", m.LongDescription})
+		if params.Metadata.LongDescription != "" {
+			raw = append(raw, []string{"text/long-desc", params.Metadata.LongDescription})
 		}
 
-		if m.Image.Bytes != nil {
-			raw = append(raw, []string{"image/" + m.Image.Ext + ";base64",
-				base64.StdEncoding.EncodeToString(m.Image.Bytes)})
-		} else if m.Image.DataURI != "" {
-			raw = append(raw, strings.SplitN(m.Image.DataURI[5:], ",", 2))
+		if params.Metadata.Image.Bytes != nil {
+			raw = append(raw, []string{"image/" + params.Metadata.Image.Ext + ";base64",
+				base64.StdEncoding.EncodeToString(params.Metadata.Image.Bytes)})
+		} else if params.Metadata.Image.DataURI != "" {
+			raw = append(raw, strings.SplitN(params.Metadata.Image.DataURI[5:], ",", 2))
 		}
 
-		if m.LightningAddress != "" {
+		if params.Metadata.LightningAddress != "" {
 			tag := "text/identifier"
-			if m.IsEmail {
+			if params.Metadata.IsEmail {
 				tag = "text/email"
 			}
-			raw = append(raw, []string{tag, m.LightningAddress})
+			raw = append(raw, []string{tag, params.Metadata.LightningAddress})
 		}
 
-		m.Encoded, _ = json.Marshal(raw)
+		enc, _ := json.Marshal(raw)
+		params.EncodedMetadata = string(enc)
 	}
 
-	return m.Encoded
+	return params.EncodedMetadata
 }
 
-func (m Metadata) MarshalJSON() ([]byte, error) {
-	return json.Marshal(string(m.Encode()))
+func (params LNURLPayParams) HashMetadata() [32]byte {
+	return sha256.Sum256([]byte(params.MetadataEncoded()))
 }
 
-func (m Metadata) Hash() [32]byte {
-	return sha256.Sum256(m.Encode())
-}
-
-func (m Metadata) HashWithPayerData(payerDataJSON string) [32]byte {
-	metadataPlusPayerData := append(m.Encode(), payerDataJSON...)
-	return sha256.Sum256(metadataPlusPayerData)
+func (params LNURLPayParams) HashWithPayerData(payerDataJSON string) [32]byte {
+	metadataPlusPayerData := params.MetadataEncoded() + payerDataJSON
+	return sha256.Sum256([]byte(metadataPlusPayerData))
 }
