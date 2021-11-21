a0554be7e70542cf53cd446aa0fc977a21f9a7c8
fiatjaf <fiatjaf@gmail.com>
2021-11-21 12:30:58 -0300

    LUD-17 support on LNURLDecode.

--- codec.go
+++ codec.go
@@ -2,28 +2,46 @@ package lnurl
 
 import (
 	"errors"
+	"net/url"
 	"strings"
 )
 
 // LNURLDecode takes a bech32-encoded lnurl string and returns a plain-text https URL.
-func LNURLDecode(lnurl string) (url string, err error) {
-	tag, data, err := Decode(lnurl)
-	if err != nil {
-		return
+func LNURLDecode(lnurl string) (string, error) {
+	switch {
+	case strings.HasPrefix(lnurl, "lnurl1"):
+		// bech32
+		tag, data, err := Decode(lnurl)
+		if err != nil {
+			return "", err
+		}
+
+		if tag != "lnurl" {
+			return "", errors.New("tag is not 'lnurl', but '" + tag + "'")
+		}
+
+		converted, err := ConvertBits(data, 5, 8, false)
+		if err != nil {
+			return "", err
+		}
+
+		return string(converted), nil
+	case strings.HasPrefix(lnurl, "lnurlp://"),
+		strings.HasPrefix(lnurl, "lnurlw://"),
+		strings.HasPrefix(lnurl, "lnurlc://"),
+		strings.HasPrefix(lnurl, "keyauth://"),
+		strings.HasPrefix(lnurl, "https://"):
+
+		u := "https://" + strings.SplitN(lnurl, "://", 2)[1]
+		if parsed, err := url.Parse(u); err == nil &&
+			strings.HasSuffix(parsed.Host, ".onion") {
+			u = "https://" + strings.SplitN(lnurl, "://", 2)[1]
+		}
+
+		return u, nil
 	}
 
-	if tag != "lnurl" {
-		err = errors.New("tag is not 'lnurl', but '" + tag + "'")
-		return
-	}
-
-	converted, err := ConvertBits(data, 5, 8, false)
-	if err != nil {
-		return
-	}
-
-	url = string(converted)
-	return
+	return "", errors.New("unrecognized lnurl format: " + lnurl)
 }
 
 // LNURLEncode takes a plain-text https URL and returns a bech32-encoded uppercased lnurl string.
