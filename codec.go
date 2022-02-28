0d58a0fce28ad4cce08c2ad77f9870475b6f49e4
fiatjaf <fiatjaf@gmail.com>
2022-02-28 15:15:11 -0300

    DecodeLNURL accepts uppercase too.

--- codec.go
+++ codec.go
@@ -6,12 +6,14 @@ import (
 	"strings"
 )
 
-// LNURLDecode takes a lowercase bech32-encoded lnurl string and returns a plain-text https URL.
-func LNURLDecode(lnurl string) (string, error) {
+// LNURLDecode takes a bech32-encoded lnurl string and returns a plain-text https URL.
+func LNURLDecode(code string) (string, error) {
+	code = strings.ToLower(code)
+
 	switch {
-	case strings.HasPrefix(lnurl, "lnurl1"):
+	case strings.HasPrefix(code, "lnurl1"):
 		// bech32
-		tag, data, err := Decode(lnurl)
+		tag, data, err := Decode(code)
 		if err != nil {
 			return "", err
 		}
@@ -26,22 +28,22 @@ func LNURLDecode(lnurl string) (string, error) {
 		}
 
 		return string(converted), nil
-	case strings.HasPrefix(lnurl, "lnurlp://"),
-		strings.HasPrefix(lnurl, "lnurlw://"),
-		strings.HasPrefix(lnurl, "lnurlc://"),
-		strings.HasPrefix(lnurl, "keyauth://"),
-		strings.HasPrefix(lnurl, "https://"):
+	case strings.HasPrefix(code, "lnurlp://"),
+		strings.HasPrefix(code, "lnurlw://"),
+		strings.HasPrefix(code, "lnurlc://"),
+		strings.HasPrefix(code, "keyauth://"),
+		strings.HasPrefix(code, "https://"):
 
-		u := "https://" + strings.SplitN(lnurl, "://", 2)[1]
+		u := "https://" + strings.SplitN(code, "://", 2)[1]
 		if parsed, err := url.Parse(u); err == nil &&
 			strings.HasSuffix(parsed.Host, ".onion") {
-			u = "https://" + strings.SplitN(lnurl, "://", 2)[1]
+			u = "https://" + strings.SplitN(code, "://", 2)[1]
 		}
 
 		return u, nil
 	}
 
-	return "", errors.New("unrecognized lnurl format: " + lnurl)
+	return "", errors.New("unrecognized lnurl format: " + code)
 }
 
 // LNURLEncode takes a plain-text https URL and returns a bech32-encoded uppercased lnurl string.
