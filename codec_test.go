6de3bbc1b794952ceba2cc0f5f1c55c147ab376a
gohumble <lngohumble@gmail.com>
2022-08-17 22:15:42 +0200

    more tests

--- codec_test.go
+++ codec_test.go
@@ -17,18 +17,21 @@ func TestLNURLDecode(t *testing.T) {
 		want    string
 		wantErr bool
 	}{
-		{desc: "simple",
-			args: args{code: "lnurl1d3h82unvwqaz7tmpwp5juenfv96x5ctx9e3k7mf0wccj7mrww4exctmsv9usxr8j98"},
+		{desc: "change_scheme",
+			args: args{code: "lnurl1d3h82unvwqaz7tmpwp5juenfv96x5ctx9e3k7mf0wccj7mrww4exctmsv9usxr8j98"}, // lnurlp://api.fiatjaf.com/v1/lnurl/pay
 			want: "https://api.fiatjaf.com/v1/lnurl/pay"},
 		{desc: "puny",
-			args: args{code: "lnurl1dp68gurn8ghj77rw95khx7r3wc6kwv3nv3uhyvmpxserscfwvdhk6tmkxyhkcmn4wfkz7urp0y6jmn9j"},
-			want: "https://xn--sxqv5g23dyr3a428a.com/v1/lnurl/pay"}, // check puny code
+			args: args{code: "lnurl1dp68gurn8ghj77rw95khx7r3wc6kwv3nv3uhyvmpxserscfwvdhk6tmkxyhkcmn4wfkz7urp0y6jmn9j"}, // https://xn--sxqv5g23dyr3a428a.com/v1/lnurl/pay
+			want: "https://xn--sxqv5g23dyr3a428a.com/v1/lnurl/pay"},                                                    // check puny code
 		{desc: "puny2",
 			args: args{code: "lnurl1dp68gurn8ghjle443052l909sxr7t8ulukgg6tnrdakj7a339akxuatjdshhqctea4v3jm"},
 			want: "https://测试假域名.com/v1/lnurl/pay"}, // check puny code
 		{desc: "puny_onion",
 			args: args{code: "lnurl1dp68gup69uh7ddvtazhetevpsljel8l9jzxjummwd9hkutmkxyhkcmn4wfkz7urp0ygc52rr"},
 			want: "http://测试假域名.onion/v1/lnurl/pay"}, // check puny onion (not sure know if this is possible)
+		{desc: "puny_onion",
+			args: args{code: "lnurl1dp68gurn8ghj7er0d4skjm3wdahxjmmw9amrztmvde6hymp0wpshjcw5kvw"},
+			want: "http://domain.onion/v1/lnurl/pay"}, // check puny onion (not sure know if this is possible)
 		{desc: "string",
 			args: args{code: "lnurl1d3h82unvjhypn2"},
 			want: "lnurl", wantErr: true}, // invalid domain name. returns error and decoded input
@@ -203,6 +206,29 @@ func TestLNURLEncodeDecode(t *testing.T) {
 			}
 			// decode string
 			dec, err := LNURLDecode(enc)
+			if err != nil {
+				if !tt.wantErr {
+					t.Errorf("LNURLDecode() error = %v, wantErr %v", err, tt.wantErr)
+				}
+				return
+			}
+			// decode string
+			dec, err = LNURLDecode(enc)
+			if err != nil {
+				if !tt.wantErr {
+					t.Errorf("LNURLDecode() error = %v, wantErr %v", err, tt.wantErr)
+				}
+				return
+			}
+			enc, err = LNURLEncode(tt.args.input)
+			if err != nil {
+				if !tt.wantErr {
+					t.Errorf("LNURLDecode() error = %v, wantErr %v", err, tt.wantErr)
+				}
+				return
+			}
+			// decode string
+			dec, err = LNURLDecode(enc)
 			if err != nil {
 				if !tt.wantErr {
 					t.Errorf("LNURLDecode() error = %v, wantErr %v", err, tt.wantErr)
